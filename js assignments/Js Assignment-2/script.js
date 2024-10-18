6a094974f7c37da4df4b69d3c8e3cfa0a4ad4b67
VestedNoman <noman@lendvest.io>
2024-10-18 13:03:26 +0500

    add 2 assignment

---
+++ js assignments/Js Assignment-2/script.js
@@ -0,0 +1,111 @@
+alert("Hello World");
+
+
+//                                                    [--ARRAYS--]
+
+//Qno:1 
+// let stds = []
+
+//Qno:2
+// let std = []
+
+
+//Qno:3
+// let arr = "[a,b,c]"
+
+//Qno:4
+// let num = [1,2,3]
+
+//Qno:5
+// let boolean = [true,false];
+
+//Qno:6
+// let mix = [1,2,3,a,b,c,true ]
+
+//Qno:7
+// let degree = [  
+//     "Qualifications in Pakistan:",
+//     "1) SSC",
+//     "2) HSC",
+//     "3) BCS",
+//     "4) BS",
+//     "5) BCOM",
+//     "6) MS",
+//     "7) M.Phil.",
+//     "8) PhD",
+// ];
+// document.write(degree[0], '<br />')
+// document.write(degree[1], '<br />')
+// document.write(degree[2], '<br />')
+// document.write(degree[3], '<br />')
+// document.write(degree[4], '<br />')
+// document.write(degree[5], '<br />')
+// document.write(degree[6], '<br />')
+// document.write(degree[7], '<br />')
+// document.write(degree[8], '<br />')
+    
+
+
+//Qno:8
+// let totalmarks = 500;
+// let std = ["Micheal","John","Tony"]
+// let score = [320,230,480]
+
+// document.write("Score of " + std[0] + " is " + score[0] + ". Percentage: " + score[0]/totalmarks*100 + "%", '<br />');
+// document.write("Score of " + std[1] + " is " + score[1] + ". Percentage: " + score[1]/totalmarks*100 + "%", '<br />');
+// document.write("Score of " + std[2] + " is " + score[2] + ". Percentage: " + score[2]/totalmarks*100 + "%" );
+
+
+
+//Qno:9
+// let colors = ["blue","red","yellow"]
+// document.write(colors, '<br />');
+
+//a
+// let addColor = prompt("What color you want to add at beginning")
+// colors.unshift(addColor);
+// document.write(colors, '<br />');
+
+//b
+// let endColor = prompt("What color you want to add at last")
+// colors.push(endColor);
+// document.write(colors, '<br />');
+
+//c
+// colors.unshift("green","black")
+// document.write(colors, '<br />');
+
+//d
+// colors.shift()
+// document.write(colors, '<br />');
+
+//e
+// colors.pop()
+// document.write(colors, '<br />');
+
+//f
+// let index = prompt("In which index you want to add color")
+// let indexColor = prompt("Write color name")
+// colors.splice(index,0,indexColor)
+// document.write(colors, '<br />');
+//g
+// let indexDel = prompt("In which index you want to add color")
+// let del = prompt("How many colors you want to delete")
+// colors.splice(indexDel,del)
+// document.write(colors);
+
+//Qno:10
+let stdScore = [320,230,480,120];
+document.write("Scores of Students:",stdScore, '<br />');
+stdScore.sort();
+document.write("Ordered Scores of Students:",stdScore);
+
+//Qno:11
+// document.write("Cities list:", '<br />')
+// let city = ["Karachi","Islamabad", "Lahore","Quetta","Peshawar"];
+// document.write(city , '<br />')
+
+// let copy = city.slice(1,3);
+// document.write(copy , '<br />')
+
+
